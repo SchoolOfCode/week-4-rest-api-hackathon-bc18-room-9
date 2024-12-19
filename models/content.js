e45edb04a3d64957792e0be36e0d9fc3e32afe5c
fergus-g <fergus.gildea@utrack.com>
2024-12-19 13:40:58 +0000

    Get all movies route working

--- models/content.js
+++ models/content.js
@@ -1,1 +1,10 @@
-export function getContent() {}
+import { data } from "../libs/data.js";
+
+let movies = data.map((movies) => ({ ...movies }));
+
+// GET
+// Gets back all the movies
+
+export function getContent() {
+  return [...movies];
+}
