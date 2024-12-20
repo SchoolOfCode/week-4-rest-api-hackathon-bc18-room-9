5856fcc2976b19fcf9149193dafe9cc7fe72cf26
Sam Annetts <samannetts8@gmail.com>
2024-12-20 10:26:08 +0000

    TV Show Router and Functions added

--- models/content.js
+++ models/content.js
@@ -8,7 +8,9 @@ let tvshows = data
   .filter((item) => item.type === "TV Show")
   .map((movie) => ({ ...movie }));
 
-export async function getContent() {
+// ===== Movie Functions =====
+
+export async function getMovieContent() {
   return [...movies];
 }
 
@@ -32,7 +34,7 @@ export async function addReviewToMovie(movieId, review_string) {
   return selected_movie;
 }
 
-export async function deleteReview(movieId) {
+export async function deleteMovieReview(movieId) {
   console.log(movieId);
   const selected_movie = movies.find(({ show_id }) => show_id === movieId);
   console.log("Found movie");
@@ -60,4 +62,60 @@ export async function removeMovieEntry(movieId) {
     .map((movie) => ({ ...movie }));
   movies = updated_movies;
   return movies;
+}
+
+// ===== TV Show Functions =====
+
+export async function getTVContent() {
+  return [...tvshows];
+}
+
+export async function getTVById(movieId) {
+  return tvshows.find(({ show_id }) => show_id === movieId);
+}
+
+export async function getTVByParam(field, value) {
+  if (field === "release_year") {
+    return tvshows.find((tvshows) => tvshows[field] === value);
+  } else {
+    return tvshows.find((tvshows) => tvshows[field].includes(value, 0));
+  }
+}
+
+export async function addReviewToTV(movieId, review_string) {
+  console.log(movieId);
+  const selected_movie = tvshows.find(({ show_id }) => show_id === movieId);
+  console.log("Found movie");
+  selected_movie["review"] = review_string;
+  return selected_movie;
+}
+
+export async function deleteTVReview(movieId) {
+  console.log(movieId);
+  const selected_movie = tvshows.find(({ show_id }) => show_id === movieId);
+  console.log("Found movie");
+  delete selected_movie["review"];
+  return selected_movie;
+}
+
+export async function createNewTV(movie_details) {
+  const current_movie_total = movies.length;
+  const current_tv_total = tvshows.length;
+  const new_movie = {
+    show_id: "s" + (current_movie_total + current_tv_total + 1),
+    ...movie_details,
+  };
+  console.log(new_movie.show_id);
+  console.log(typeof new_movie.show_id);
+  movies = [...tvshows, new_movie];
+  return new_movie;
+}
+
+export async function removeTVEntry(movieId) {
+  console.log(movieId);
+  const updated_tvshows = tvshows
+    .filter((item) => item.show_id != movieId)
+    .map((movie) => ({ ...movie }));
+  tvshows = updated_tvshows;
+  return tvshows;
 }
